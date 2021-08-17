a77043ce6b1a578f001380936596e1e3ac9509b4
ayham <learn.ayham@zoho.com>
2021-08-17 15:55:52 +0200

    adding test range and test price

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,7 +59,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data):
+    """
+    ensure that row count is withen limt
+    """
+    assert 15000 < data.shape[0] < 1000000
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_price_range(data, min_price, max_price):
+    """
+    ensure that price is withen accepted range
+    """
+    assert data['price'].between(min_price,max_price)
+
