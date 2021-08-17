81e3d504b51acbce72354d4517d1f8dcf0b6a02c
ayham <learn.ayham@zoho.com>
2021-08-17 15:25:46 +0200

    test pass download and clean data

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,82 +1,103 @@
 #!/usr/bin/env python
 """
-long_description [An example of a step using MLflow and Weights & Biases]: Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+Download from W&B the raw dataset and apply some basic data cleaning, 
+exporting the result to a new artifact
 """
 import argparse
 import logging
 import wandb
+import os
 
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
 def go(args):
-
-    run = wandb.init(job_type="job_type [my_step]: basic_cleaning")
+    '''
+    updating input_artifact after basic cleaning
+    '''
+    
+    run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    logger.info("Downloading artifact")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+        
+    df = pd.read_csv(artifact_local_path, low_memory=False)
+   
+    logger.info("basic cleaning ... removing outliers")
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    
+    filename = "clean_sample.csv"
+    df.to_csv(filename, index=False)
+
+    #update wandb
+    artifact = wandb.Artifact(
+     args.output_artifact,
+     type=args.output_type,
+     description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+
+    logger.info("Logging artifact")
+    run.log_artifact(artifact)
+
+    os.remove(filename)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="short_description [My step]: A very basic data cleaning")
-
-
-    parser.add_argument(
-        "--parameters [parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
-    )
+    parser = argparse.ArgumentParser(
+        description="basic data cleaning",
+        fromfile_prefix_chars="@",
+        )
 
     parser.add_argument(
-        "--parameter2]: input_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--input_artifact", 
+        type=str,
+        help="Fully-qualified name for the input artifact",
         required=True
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        type=str,
+        help="Fully-qualified name for the input artifact",
+        required=True,
     )
 
     parser.add_argument(
-        "--output_type", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        "--output_type", type=str, help="Type for the artifact", required=True,
     )
 
     parser.add_argument(
         "--output_description", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        type=str,
+        help="Description for the artifact",
+        required=True,
     )
 
     parser.add_argument(
         "--min_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        type=float,
+        help="Hueristic for minimum accepted price",
+        required=True,
     )
 
     parser.add_argument(
         "--max_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        type=float,
+        help="Hueristic for minimum accepted price",
+        required=True,
     )
 
 
